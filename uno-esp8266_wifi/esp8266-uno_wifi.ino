898a2b7d29160d1bd0b128b856ae988140294aa8
Bugs Bunny Productions <37730925+htpbbp@users.noreply.github.com>
2018-05-13 15:21:25 +0200

    Update esp8266-uno_wifi.ino

--- uno-esp8266_wifi/esp8266-uno_wifi.ino
+++ uno-esp8266_wifi/esp8266-uno_wifi.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "Site_2";
-const char* password = "HaseRo2B";
+const char* ssid = "your_wifi";
+const char* password = "your_wifi_passwd";
 
 ESP8266WebServer server(80);
 
